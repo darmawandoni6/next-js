35c739104870eb7ff683b49c3d34db17af5fd40c
darmawandoni6 <darmawandoni6@gmail.com>
2020-05-19 16:59:46 +0700

    tes2

--- next.config.js
+++ next.config.js
@@ -8,53 +8,20 @@ const webpack = require("webpack");
 const DotEnv = require("dotenv");
 const path = require("path");
 
-module.exports = withPlugins(
-  [
-    // [
-    //   optimizedImages,
-    //   {
-    //     inlineImageLimit: 8192,
-    //     imagesFolder: "images",
-    //     imagesName: "[name]-[hash].[ext]",
-    //     handleImages: ["jpeg", "jpg", "png", "svg", "webp", "gif", "ico"],
-    //     optimizeImages: true,
-    //     optimizeImagesInDev: false,
-    //     mozjpeg: {
-    //       quality: 80,
-    //     },
-    //     optipng: {
-    //       optimizationLevel: 3,
-    //     },
-    //     pngquant: false,
-    //     gifsicle: {
-    //       interlaced: true,
-    //       optimizationLevel: 3,
-    //     },
-    //     webp: {
-    //       preset: "default",
-    //       quality: 75,
-    //     },
-    //   },
-    // ],
-    [withCss],
-    [withSass],
-    [withImages],
-  ],
-  {
-    webpack: (config) => {
-      config.plugins.push(new webpack.EnvironmentPlugin(process.env));
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "@actions": path.resolve("src/store/actions"),
-        "@components": path.resolve("src/components"),
-        "@containers": path.resolve("src/containers"),
-        "@configs": path.resolve("src/configs"),
-        "@helpers": path.resolve("src/helpers"),
-        "@images": path.resolve("src/assets/images"),
-        "@styles": path.resolve("src/assets/styles"),
-        "@reducers": path.resolve("src/store/reducers"),
-      };
-      return config;
-    },
-  }
-);
+module.exports = withPlugins([[withCss], [withSass], [withImages]], {
+  webpack: (config) => {
+    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@actions": path.resolve("src/store/actions"),
+      "@components": path.resolve("src/components"),
+      "@containers": path.resolve("src/containers"),
+      "@configs": path.resolve("src/configs"),
+      "@helpers": path.resolve("src/helpers"),
+      "@images": path.resolve("src/assets/images"),
+      "@styles": path.resolve("src/assets/styles"),
+      "@reducers": path.resolve("src/store/reducers"),
+    };
+    return config;
+  },
+});
